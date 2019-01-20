c9366ab0d3a386457cc8c426b382fa5c4aed8b65
SiegfriedWagner <mateus.chojnowski@gmail.com>
2019-01-21 00:15:06 +0100

    example seed migration

--- Model/Entities/Price.cs
+++ Model/Entities/Price.cs
@@ -9,7 +9,7 @@ namespace Model.Entities
     public class Price
     {
         public int Id { get; set; }
-        public int Value { get; set; }
+        public decimal Value { get; set; }
         public DateTime CreationDate { get; set; }
         public virtual Product Product { get; set; }
     }
